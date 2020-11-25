1547040e2b28bf8d663bcc522288f359acb6893a
Manohar <manoharsomu@gmail.com>
2020-11-25 12:13:44 +0530

    added Redux Persistor

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import  {store, persistor} from './redux/store';  
+
 
 ReactDOM.render(
  <Provider store={store}>  
      <BrowserRouter> 
+      <PersistGate persistor ={persistor}>
         <App />
+        </PersistGate>
     </BrowserRouter>
     </Provider>
 
